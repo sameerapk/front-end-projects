8943f2ddbb8036879c73209f231a90eb90c0e413
Sameera <sameerakmurthy@gmail.com>
2018-05-17 04:33:05 +0530

    Add files via upload
    Javascript project

---
+++ Ticky clok/javascript/clock.js
@@ -0,0 +1,27 @@
+var d,h,m,s,clock,clr;
+function clocky(){
+    d=new Date();
+    h=d.getHours();
+    m=d.getMinutes();
+    s=d.getSeconds();
+    
+if(h<=9){
+    h='0' + h; 
+}
+    if(m<=9){
+    m='0' + m; 
+}
+
+    if(s<=9){
+    s='0' + s; 
+}
+clock=h + ':' + m + ':' + s;
+clr='#'+h+m+s;
+    
+document.getElementById('clock').innerHTML=clock;
+document.body.style.background=clr;
+
+setTimeout(clocky,1000);
+    
+}
+clocky();
